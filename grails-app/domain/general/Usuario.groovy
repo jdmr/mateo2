9f016339b91444e2832d8bcd8259f68d43f3a390
J. David Mendoza <jdmendoza@um.edu.mx>
2011-05-31 12:07:37 -0500

    Instalacion inicial del plugin de seguridad

---
+++ grails-app/domain/general/Usuario.groovy
@@ -0,0 +1,24 @@
+package general
+
+class Usuario {
+
+	String username
+	String password
+	boolean enabled
+	boolean accountExpired
+	boolean accountLocked
+	boolean passwordExpired
+
+	static constraints = {
+		username blank: false, unique: true
+		password blank: false
+	}
+
+	static mapping = {
+		password column: '`password`'
+	}
+
+	Set<Rol> getAuthorities() {
+		UsuarioRol.findAllByUsuario(this).collect { it.rol } as Set
+	}
+}
